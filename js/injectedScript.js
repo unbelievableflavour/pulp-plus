1dababbb94b4a274c7560634a0979774410c16b6
Bart Zaalberg <b.zaalberg@simgroep.nl>
2022-05-11 09:05:04 +0200

    Added observer for script-select

--- js/injectedScript.js
+++ js/injectedScript.js
@@ -254,6 +254,20 @@ if ( scriptSelectPlayerPriority.enabled ) {
 
   scriptSelect.prepend(player)
   scriptSelect.prepend(label)
+
+  function scriptSelectPlayerPriorityCallback(mutations) {
+    if ( scriptSelect.children[0] === label ) {
+      return;
+    }
+
+    scriptSelect.prepend(player)
+    scriptSelect.prepend(label)
+  }
+
+  let observer = new MutationObserver(scriptSelectPlayerPriorityCallback);
+  observer.observe(scriptSelect, {
+    childList: true,
+  });
 }
 
 if ( removeFooter.enabled ) {
