e399eae252f7cba5dee0f3b707ec7794b29d296a
Bart Zaalberg <b.zaalberg@simgroep.nl>
2022-05-11 09:33:44 +0200

    Added screenshotting

--- js/injectedScript.js
+++ js/injectedScript.js
@@ -36,6 +36,9 @@ const defaultConfig = {
   },
   scriptSelectPlayerPriority: {
     enabled: true
+  },
+  screenshot: {
+    enabled: true
   }
 }
 
@@ -77,10 +80,49 @@ const {
   responsiveScriptView,
   prettyPrintExportButtons,
   openScriptButton,
+  screenshot,
   customTheme,
   removeFooter,
   scriptSelectPlayerPriority,
 } = updatedConfig;
+
+if ( screenshot.enabled ) {
+  const script = createElement('script', {
+    src: "https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js"
+  });
+
+  document.head.appendChild(script)
+
+  function saveScreenshot(canvas) {
+    const fileName = "pulp-screenshot";
+    const link = document.createElement("a");
+    link.download = fileName + ".png";
+    console.log(canvas);
+    canvas.toBlob(function (blob) {
+      console.log(blob);
+      link.href = URL.createObjectURL(blob);
+      link.click();
+    });
+  }
+
+  function screenshotRoom(){
+    let div = one('#room-pane .grid');
+    html2canvas(div).then(saveScreenshot)
+  }
+
+  const button = createElement('button', {
+    type: "button",
+    onclick: screenshotRoom,
+    innerText: "Screenshot room"
+  });
+
+  const buttonContainer = createElement('div', {
+    className: "wrap",
+  });
+
+  buttonContainer.appendChild(button);
+  one('#room-pane').appendChild(buttonContainer);
+}
 
 if ( biggerSearchResults.enabled ) {
   const styles = `
